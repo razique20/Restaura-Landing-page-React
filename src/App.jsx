64f771d22c562a22ef17f6e9e9021400762c8125
Razique Mk <raziquemk2002@gmail.com>
2024-08-15 21:08:16 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
 // import "./Index.css"
 
 const App = () => {
   return (
-    <div>
+    <main className='overflow-y-hidden text-neutral-200 antialiased'>
       <HeroSection/>
-    </div>
+      <Navbar/>
+    </main>
   )
 }
 
