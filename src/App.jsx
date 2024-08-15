c5b939a793a85002ef21ae34a6a6d51eea861976
Razique Mk <raziquemk2002@gmail.com>
2024-08-15 15:58:00 +0530

    intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+// import "./Index.css"
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection/>
+    </div>
+  )
+}
+
+export default App
