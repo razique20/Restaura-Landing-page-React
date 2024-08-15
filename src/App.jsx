2465f4a2e0d5c60a70e961d93bbe0b333d032737
Razique Mk <raziquemk2002@gmail.com>
2024-08-15 22:39:32 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import Dishes from './components/Dishes'
+import About from './components/About'
+import Mission from './components/Mission'
 // import "./Index.css"
 
 const App = () => {
@@ -8,7 +11,10 @@ const App = () => {
     <main className='overflow-y-hidden text-neutral-200 antialiased'>
       <HeroSection/>
       <Navbar/>
-    </main>
+      <Dishes/>
+      <About/>
+      <Mission/>
+         </main>
   )
 }
 
---
+++ src/components/Mission.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Mission = () => {
+  return (
+    <section id='mission'>
+        <div className='container mx-auto text-center '>
+            <h2 className='mb-8 text-center lg:text-4xl'>Our Mission</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Mission
